27f26f59e0a190746735dc802bfd0b374ce67fd7
DESKTOP-SLPQ3FR\Toni <sultonihawk732@gmail.com>
2018-05-30 10:57:12 +0700

    Sudah aku check di projeck lain, bisa jalan udah datenya

---
+++ app/src/main/java/com/klanrock/klanrock/setTime.java
@@ -0,0 +1,72 @@
+package com.klanrock.klanrock;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Hafizh Herdi on 1/21/2017.
+ */
+
+public class setTime extends AppCompatActivity {
+
+    private TextView tvTimeResult;
+    private ImageView btTimePicker;
+    private TimePickerDialog timePickerDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datapaket);
+
+        tvTimeResult = (TextView) findViewById(R.id.jam);
+        btTimePicker = (ImageView) findViewById(R.id.jam_alt);
+        btTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimeDialog();
+            }
+        });
+    }
+
+    private void showTimeDialog() {
+
+        /**
+         * Calendar untuk mendapatkan waktu saat ini
+         */
+        Calendar calendar = Calendar.getInstance();
+
+        /**
+         * Initialize TimePicker Dialog
+         */
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                /**
+                 * Method ini dipanggil saat kita selesai memilih waktu di DatePicker
+                 */
+                tvTimeResult.setText("Waktu dipilih = "+hourOfDay+":"+minute);
+            }
+        },
+                /**
+                 * Tampilkan jam saat ini ketika TimePicker pertama kali dibuka
+                 */
+                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),
+
+                /**
+                 * Cek apakah format waktu menggunakan 24-hour format
+                 */
+                DateFormat.is24HourFormat(this));
+
+        timePickerDialog.show();
+    }
+}
